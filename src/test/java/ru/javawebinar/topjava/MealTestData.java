b8582f189aed402f4b4b8732348bfc2734b83a7e
Maks <avto12@i.ua>
2015-10-11 15:05:40 +0300

    hw4 without optional

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -6,10 +6,6 @@ import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.model.UserMeal;
 
 import java.time.LocalDateTime;
-
-import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
-
-import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
@@ -29,9 +25,9 @@ public class MealTestData {
     public static final User USER = new User(USER_ID, "User", "user@yandex.ru", "password", Role.ROLE_USER);
     public static final User ADMIN = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ROLE_ADMIN);
 
-    public static UserMeal userMeal1 =new UserMeal(100002,LocalDateTime.parse("2015-10-24 09:00:00"), "завтрак",1000);
-    public static UserMeal userMeal2 =new UserMeal(100003,LocalDateTime.parse("2015-10-24 09:00:00"), "обед",1000);
-    public static UserMeal userMeal3 =new UserMeal(100004,LocalDateTime.parse("2015-10-24 09:00:00"), "завтрак",1000);
+    public static UserMeal userMeal1 =new UserMeal(100002,LocalDateTime.parse("2015-10-24T09:00:00"), "завтрак",1000);
+    public static UserMeal userMeal2 =new UserMeal(100003,LocalDateTime.parse("2015-10-24T09:00:00"), "обед",1000);
+    public static UserMeal userMeal3 =new UserMeal(100004,LocalDateTime.parse("2015-10-24T09:00:00"), "завтрак",1000);
 
     public static final ModelMatcher<UserMeal, String> MATCHER = new ModelMatcher<>(UserMeal::toString);
 
