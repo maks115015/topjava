bb9202e9e183920a24619eb1359c4c7fbefb51a8
Maks <avto12@i.ua>
2015-11-05 23:55:15 +0200

    hw7 patched

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
@@ -9,6 +9,7 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
@@ -28,6 +29,7 @@ import java.util.stream.Collectors;
  */
 
 @Repository
+@Transactional(readOnly = true)
 public class JdbcUserRepositoryImpl implements UserRepository {
 
     private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
@@ -49,6 +51,7 @@ public class JdbcUserRepositoryImpl implements UserRepository {
     }
 
     @Override
+    @Transactional
     public User save(User user) {
         MapSqlParameterSource map = new MapSqlParameterSource()
                 .addValue("id", user.getId())
@@ -74,6 +77,7 @@ public class JdbcUserRepositoryImpl implements UserRepository {
     }
 
     @Override
+    @Transactional
     public boolean delete(int id) {
         return jdbcTemplate.update("DELETE FROM users WHERE id=?", id) != 0;
     }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -7,17 +7,23 @@
 <body>
 <jsp:include page="fragments/bodyHeader.jsp"/>
 <section>
-    <form method="post" action="users">
-    <fmt:message key="app.login"/>: <select name="userId">
-    <option value="1" selected>User</option>
-    <option value="2">Admin</option>
-</select>
-    <button type="submit"><fmt:message key="common.select"/></button>
-</form>
-<ul>
-    <li><a href="users"><fmt:message key="users.title"/></a></li>
-    <li><a href="meals"><fmt:message key="meals.title"/></a></li>
-</ul>
+<div class="jumbotron">
+    <div class="container">
+        <p/>
+
+        <form method="post" action="users">
+            <fmt:message key="app.login"/>: <select name="userId">
+            <option value="100000" selected>User</option>
+            <option value="100001">Admin</option>
+        </select>
+            <button type="submit"><fmt:message key="common.select"/></button>
+        </form>
+        <ul>
+            <li><a href="users"><fmt:message key="users.title"/></a></li>
+            <li><a href="meals"><fmt:message key="meals.title"/></a></li>
+        </ul>
+    </div>
+</div>
 </section>
 <jsp:include page="fragments/footer.jsp"/>
 </body>
